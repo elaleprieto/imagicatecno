b9d207c9f0d989a8240894a703ce1d6828a9d685
Ale Prieto <aleprieto@gmail.com>
2013-08-11 18:21:46 -0300

    analitycs

--- templates/strapped/index.php
+++ templates/strapped/index.php
@@ -267,6 +267,16 @@ endif;
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap.min.js"></script> 
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/application.js"></script> 
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/prettify.js"></script> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-27799433-8', 'pueblo.coop');
+  ga('send', 'pageview');
+
+</script>
 <script type="text/javascript">
         jQuery.noConflict();
 </script>
